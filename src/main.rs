ea926055c8cbaf2116a7ec4962e7f763f6a6f6e1
jimmy <jimmy@izhong>
2022-12-02 17:41:23 +0800

    default

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ struct Opt {
     #[clap(short = 'o', long = "log")]
     log: Option<String>,
 
-    /// Generate a new address xversion=3
+    /// Generate a new address xversion=4
     #[clap(long = "new-address")]
     new_address: bool,
 
